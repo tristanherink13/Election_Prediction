c8dd1f7f3e28b004ed6af135b3bf3c2bdd1c8600
tristanherink13 <herink.tristan@gmail.com>
2020-08-27 18:40:40 -0400

    clean up code, move electoral votes to txt file

--- election_data_manipulation.py
+++ election_data_manipulation.py
@@ -4,6 +4,7 @@ import os
 import sys
 import pandas as pd
 import csv
+import ast
 import geopandas as gpd
 import numpy as np
 from shapely.geometry import Point, Polygon
@@ -13,14 +14,13 @@ import matplotlib
 import time
 
 # get file path
-pd_file_path = os.path.join(sys.path[0], 'election_data_1976_2016', '1976-2016-president.csv')
+pd_file_path = os.path.join(sys.path[0], 'Datasets', 'election_data_1976_2016', '1976-2016-president.csv')
 # read in CSV
 df = pd.read_csv(pd_file_path)
 # drop writeins
 df = df[df.writein != True]
 # drop irrelevant columns
 df = df.drop(columns=['state_cen', 'version', 'notes', 'writein', 'state_ic', 'office'])
-    
 
 # rename column to match with other dataframe for merging later on
 df = df.rename(columns = {'state_po':'STUSPS'})
@@ -29,7 +29,6 @@ unique_abbrevs = df['STUSPS'].unique().tolist()
 
 # group by year, party, and state abbreviation
 year_party_grouping = df.groupby(['year', 'party', 'STUSPS'])['candidatevotes'].sum()
-
 
 # initialize lists
 years = [1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016]
@@ -82,44 +81,22 @@ for i in range(0, len(winner_list), 51):
 # structure == {year:{state:winner}}
 winner_dict = dict(zip(years, winner_dict_list))
 
-
-def state_plotter(states, year, winner_dict, df, us_map=True):
-# setting up us_map as an input. if you want to return states that are spred out, leave it set to true.
-# if you're plotting bordering states and prefer to zoom in. 
+def state_plotter(states, year, winner_dict, df):
 
     ### plot data on US map (https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html)
-    gpd_file_path = os.path.join(sys.path[0], 'shapefile_mapping', 'cb_2018_us_state_500k.shp')
+    gpd_file_path = os.path.join(sys.path[0],'Datasets', 'shapefile_mapping', 'cb_2018_us_state_500k.shp')
     usa = gpd.read_file(gpd_file_path)
 
-    # drop mappings (including Alaska + Hawaii for aesthetics)
+    # drop irrelevant mappings
     usa = usa[(usa.NAME != 'United States Virgin Islands') & (usa.NAME != 'Guam') 
             & (usa.NAME != 'Puerto Rico')& (usa.NAME != 'American Samoa')
             & (usa.NAME != 'Commonwealth of the Northern Mariana Islands')]
 
-    # create winner series
+    # create winner series and group df to ascertain popular vote count for both parties
     winner_series  = pd.Series(winner_dict[year])
-
-    # create dicts for dem and rep nominees and extract relevant data
-    
     
     year_winner_grouping = df.groupby(['year', 'party', 'candidate'])['candidatevotes'].sum()
-
     all_popular_votes_df = df.groupby(['year', 'candidate'])['candidatevotes'].sum()
-
-    #year_winner_grouping[].nlargest(2)
-
-
-    #print(year_winner_grouping[2000]['democrat'])
-
-
-    #print(another_grouping[2000]['Gore, Al'])
-    #final_usa['STUSPS'] == state
-
-    #s = year_winner_grouping[2000]
-
-    #print(s)
-
-    #print(type(locate_person_df))
 
     dem_grouping = year_winner_grouping[year]['democrat']
     rep_grouping = year_winner_grouping[year]['republican']
@@ -129,53 +106,10 @@ def state_plotter(states, year, winner_dict, df, us_map=True):
     dem_pop_vote = all_popular_votes_df[year][dem_candidate]
     rep_pop_vote = all_popular_votes_df[year][rep_candidate]
 
-    #dem_nom_dict = [dict(candidate=k, votes=v) for k, v in dem_grouping.items()]
-    #rep_nom_dict = [dict(candidate=k, votes=v) for k, v in rep_grouping.items()]
-
-
-    # create dict for num of electoral college votes/state/yr
-    electoral_votes_per_state_per_year_dict = {
-                                              1976:[9,3,6,6,45,7,8,3,3,17,12,4,4,26,13,
-                                              8,7,9,10,4,10,14,21,10,7,12,4,5,3,4,17,
-                                              4,41,13,3,25,8,6,27,4,8,4,10,26,4,3,12,
-                                              9,6,11,3],
-                                              1980:[9,3,6,6,45,7,8,3,3,17,12,4,4,26,13,
-                                              8,7,9,10,4,10,14,21,10,7,12,4,5,3,4,17,4,
-                                              41,13,3,25,8,6,27,4,8,4,10,26,4,3,12,9,6,
-                                              11,3],
-                                              1984:[9,3,7,6,47,8,8,3,3,21,12,4,4,24,12,
-                                              8,7,9,10,4,10,13,20,10,7,11,4,5,4,4,16,5,
-                                              36,13,3,23,8,7,25,4,8,3,11,29,5,3,12,10,
-                                              6,11,3],
-                                              1988: [9,3,7,6,47,8,8,3,3,21,12,4,4,24,12,
-                                              8,7,9,10,4,10,13,20,10,7,11,4,5,4,4,16,5,
-                                              36,13,3,23,8,7,25,4,8,3,11,29,5,3,12,10,
-                                              6,11,3],
-                                              1992: [9,3,8,6,54,8,8,3,3,25,13,4,4,22,12,
-                                              7,6,8,9,4,10,12,18,10,7,11,3,5,4,4,15,5,
-                                              33,14,3,21,8,7,23,4,8,3,11,32,5,3,13,11,
-                                              5,11,3],
-                                              1996: [9,3,8,6,54,8,8,3,3,25,13,4,4,22,12,
-                                              7,6,8,9,4,10,12,18,10,7,11,3,5,4,4,15,5,
-                                              33,14,3,21,8,7,23,4,8,3,11,32,5,3,13,11,
-                                              5,11,3],
-                                              2000: [9,3,8,6,54,8,8,3,3,25,13,4,4,22,12,
-                                              7,6,8,9,4,10,12,18,10,7,11,3,5,4,4,15,5,
-                                              33,14,3,21,8,7,23,4,8,3,11,32,5,3,13,11,
-                                              5,11,3],
-                                              2004: [9,3,10,6,55,9,7,3,3,27,15,4,4,21,11,
-                                              7,6,8,9,4,10,12,17,10,6,11,3,5,5,4,15,5,31,
-                                              15,3,20,7,7,21,4,8,3,11,34,5,3,13,11,5,10,3],
-                                              2008: [9,3,10,6,55,9,7,3,3,27,15,4,4,21,11,
-                                              7,6,8,9,4,10,12,17,10,6,11,3,5,5,4,15,5,31,
-                                              15,3,20,7,7,21,4,8,3,11,34,5,3,13,11,5,10,3],
-                                              2012: [9,3,11,6,55,9,7,3,3,29,16,4,4,20,11,
-                                              6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,
-                                              15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3],
-                                              2016: [9,3,11,6,55,9,7,3,3,29,16,4,4,20,11,
-                                              6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,
-                                              15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3]
-    }
+    # read in dict with electoral votes per state per year
+    electoral_path = os.path.join(sys.path[0], 'Datasets', 'electoral_votes_per_state.txt')
+    with open(electoral_path, 'r') as data:
+        electoral_votes_per_state_per_year_dict = ast.literal_eval(data.read())
 
     # index votes/state based on input year
     electoral_votes_per_state = electoral_votes_per_state_per_year_dict[year]
@@ -197,34 +131,14 @@ def state_plotter(states, year, winner_dict, df, us_map=True):
     usa['WINNER'] = winner_ordered_list
     usa['VOTES'] = electoral_votes_ordered_list
 
-
     # instantiate a matplotlib figure
-    # to change size, use x_lim and y_lim. changing the figsize will not change the size of the map
-    
-    fig, ax = plt.subplots(figsize=(20,8))#, frameon=False)
-
-
-    # Create a Rectangle patch
-    rect = mpatches.Rectangle(
-                             (1,1),30,40,linewidth=1,edgecolor='r',
-                             fill=False,clip_on=False,clip_box=(.97, 1.01))#,
-                             #zorder=2)#, set_bounds(left, bottom, width, height))
-    
-    #plt.legend(handles=[rect], title='Voting Statistics',loc='upper left')
-    #ax.add_patch(rect)
-
+    fig, ax = plt.subplots(figsize=(20,8))
 
     # shift states plot over to left
     ax = fig.add_axes([0.02, 0.02, 0.8, 0.8])
 
     # remove axis around map
     fig.subplots_adjust(wspace=0, top=1, right=1, left=0, bottom=0)
-
-    ax.annotate(rect, (100,100), color='black', weight='bold', 
-                fontsize=6, ha='center', va='center')
-
-    # Add the patch to the Axes
-    #ax2.add_patch(rect)
 
     ax.set_title('Presidential Winner By State, {}'.format(year), fontsize=14, x=.625)
     
@@ -234,16 +148,6 @@ def state_plotter(states, year, winner_dict, df, us_map=True):
     ymin = 15
     ymax = 75
 
-
-    
-    
-    #left = 10
-    #bottom = 10
-    #width = 10
-    #height = 10
-    #ax = fig.add_axes((left, bottom, width, height))
-    
-    
     handles, labels = ax.get_legend_handles_labels()
 
     # choose color scheme and line thickness for dem/rep
@@ -323,54 +227,38 @@ def state_plotter(states, year, winner_dict, df, us_map=True):
     ax.add_artist(first_pt_2_legend)
     ax.add_artist(second_legend)
 
-    if us_map:
-
     # the following series of if/elif/else statements provide control over
     # whether Alaska and Hawaii will show up in the map. Because of their 
     # distance from the lower 48, and the size of Alaska, we don't want them
     # in the map unless necessary
 
-        if 'AK' and 'HI' in states:
-            fortyeight = False
-            ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
-        elif 'AK' in states:
-            final_usa = usa[usa.STUSPS != 'HI']
-            ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
-        elif 'HI' in states:
-            final_usa = usa[usa.STUSPS != 'AK']
+    if 'AK' and 'HI' in states:
+        fortyeight = False
+        ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    elif 'AK' in states:
+        final_usa = usa[usa.STUSPS != 'HI']
+        ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
+    elif 'HI' in states:
+        final_usa = usa[usa.STUSPS != 'AK']
+    else:
+        final_usa = usa[(usa.STUSPS != 'AK') & (usa.STUSPS != 'HI')]
+
+    # go through the list of input state abbreviations and plot them
+    for state in states:
+        if fortyeight:
+            if final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'BLUE':
+                final_usa[final_usa.STUSPS == state].plot(ax=ax, color=rep_color, edgecolor='black', linewidth=line_thick).axis('off')
+            elif final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'RED':
+                final_usa[final_usa.STUSPS == state].plot(ax=ax, color=dem_color, edgecolor='black', linewidth=line_thick).axis('off')
         else:
-            final_usa = usa[(usa.STUSPS != 'AK') & (usa.STUSPS != 'HI')]
-    
-        # the following loop will go through the list of input state abbreviations and plot them
-        # ax=ax makes the states appear on the initial matplotlib figure
-        for state in states:
-            if fortyeight:
-                if final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'BLUE': # tab20, tab20c, tab10
-                    final_usa[final_usa.STUSPS == state].plot(ax=ax, color=rep_color, edgecolor='black', linewidth=line_thick).axis('off')#.tight_layout()
-                elif final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'RED': # RdBu
-                    final_usa[final_usa.STUSPS == state].plot(ax=ax, color=dem_color, edgecolor='black', linewidth=line_thick).axis('off')#.tight_layout()
-            else:
-                if usa.loc[usa['STUSPS'] == state]['WINNER'].values[0] == 'BLUE':
-                    usa[usa.STUSPS == state].plot(ax=ax, color=rep_color, edgecolor='b', linewidth=line_thick)#, column='VOTES')
-                elif usa.loc[usa['STUSPS'] == state]['WINNER'].values[0] == 'RED':
-                    usa[usa.STUSPS == state].plot(ax=ax, color=dem_color, edgecolor='r', linewidth=line_thick)#, column='VOTES')
+            if usa.loc[usa['STUSPS'] == state]['WINNER'].values[0] == 'BLUE':
+                usa[usa.STUSPS == state].plot(ax=ax, color=rep_color, edgecolor='b', linewidth=line_thick)
+            elif usa.loc[usa['STUSPS'] == state]['WINNER'].values[0] == 'RED':
+                usa[usa.STUSPS == state].plot(ax=ax, color=dem_color, edgecolor='r', linewidth=line_thick)
 
-        plt.show()
-
-    # if you choose not to have the first layer of the whole US, this will plot states on their own
-    elif us_map == False:
-        #final_usa = usa[(usa.STUSPS != 'AK') & (usa.STUSPS != 'HI')]
-        final_usa = usa.drop(usa[usa.STUSPS == 'AK'])
-        print(final_usa)
-        #for state in states:
-        #    if final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'BLUE':
-        #        final_usa[final_usa.STUSPS == state].plot(ax=ax, cmap='plasma', edgecolor='b', linewidth=2, column='VOTES')
-        #    elif final_usa.loc[final_usa['STUSPS'] == state]['WINNER'].values[0] == 'RED':
-        #        final_usa[final_usa.STUSPS == state].plot(ax=ax, cmap='RdBu', edgecolor='r', linewidth=2, column='VOTES')
-        #plt.show()
+    plt.show()
 
 #state_plotter(['TX', 'CA', 'WA', 'VT'], 1976, winner_dict, df)
-new_list = []
-[new_list.append(st) for st in unique_abbrevs if st != 'AK' and st != 'HI']
-state_plotter(new_list, 1984, winner_dict, df)#, us_map=False)
-#state_plotter(unique_abbrevs, us_map = False)
+main_states_list = []
+[main_states_list.append(st) for st in unique_abbrevs if st != 'AK' and st != 'HI']
+state_plotter(main_states_list, 1976, winner_dict, df)
---
+++ explore_kaggle_data.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from sklearn import svm
+
+### going to first start with Linear SVC bc we're predicting a category based on a <100k labeled dataset (108 columns)
+
+class Dataset:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        print('importing {} dataset...'.format(self.file_path.split('/')[-1]))
+
+    def convert_to_df(self):
+        self.df = pd.read_csv(self.file_path)
+        return self.df
+    
+    def get_df_columns(self):
+        self.df_columns = self.df.columns.to_list()
+        return self.df_columns
+
+# instantiate objects
+test_object = Dataset(os.path.join(sys.path[0], 'kaggle_2016_dataset', 'test2016.csv'))
+train_object = Dataset(os.path.join(sys.path[0], 'kaggle_2016_dataset', 'train2016.csv'))
+
+# convert object to df
+test_df = test_object.convert_to_df()
+train_df = train_object.convert_to_df()
+
+# get column headers of df
+test_columns = test_object.get_df_columns()
+train_columns = train_object.get_df_columns()
