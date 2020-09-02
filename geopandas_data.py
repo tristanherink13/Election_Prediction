bf1a805c53e07f851a8b75d95e9041ac1cbd7e51
tristanherink13 <herink.tristan@gmail.com>
2020-09-02 16:08:40 -0400

    create GeopandasData subclass from Dataset

---
+++ geopandas_data.py
@@ -0,0 +1,11 @@
+import geopandas as gpd
+from dataset import Dataset
+
+class GeopandasData(Dataset):
+
+    def manipulate_geopandas(self):
+        self.gpd = gpd.read_file(self.file_path)
+        # drop irrelevant mappings
+        self.gpd = self.gpd[(self.gpd.NAME != 'United States Virgin Islands') & (self.gpd.NAME != 'Guam') 
+                          & (self.gpd.NAME != 'Puerto Rico')& (self.gpd.NAME != 'American Samoa')
+                          & (self.gpd.NAME != 'Commonwealth of the Northern Mariana Islands')]
