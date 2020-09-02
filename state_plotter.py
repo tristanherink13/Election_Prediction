7ce11bea1506d51c4185d0007882e46f5b81435e
tristanherink13 <herink.tristan@gmail.com>
2020-09-02 18:05:05 -0400

    create StatePlotter parent class

---
+++ state_plotter.py
@@ -0,0 +1,18 @@
+from historical_state_voting_calculations import HistoricalStateVotingCalculations
+
+class StatePlotter:
+
+    def __init__(self, year):
+        # initialize attributes
+        self.year = year
+        self.main_states_list = []
+        # get StateVotingCalculations attributes
+        self.calculations = HistoricalStateVotingCalculations()
+        self.winner_method = self.calculations.determine_historical_winner(self.year)
+        # set attributes
+        self.winner_ordered_list = self.calculations.winner_ordered_list
+        self.state_ordered_list = self.calculations.state_ordered_list
+        self.electoral_votes_ordered_list = self.calculations.electoral_votes_ordered_list
+        self.usa = self.calculations.usa
+
+        print('plotting data...')
