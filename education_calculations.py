f39ad297afef43d38a468860c410899bb812e8b9
tristanherink13 <herink.tristan@gmail.com>
2020-09-08 21:24:50 -0400

    EducationCalculations subclass from Calculations

---
+++ education_calculations.py
@@ -0,0 +1,8 @@
+from calculations import Calculations
+
+class EducationCalculations(Calculations):
+
+    def group_overall_education(self):
+        pass
+        #overall_education_df = self.overall_education_df
+        #print(overall_education_df)
