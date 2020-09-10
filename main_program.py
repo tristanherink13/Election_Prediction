48c582e06f95acf3dea42141435d98bb1c25435d
tristanherink13 <herink.tristan@gmail.com>
2020-09-10 18:20:14 -0400

    user input improvement

--- main_program.py
+++ main_program.py
@@ -1,6 +1,9 @@
+import sys
 import argparse
 
 from state_map import StateMap
+from calculations import Calculations
+from prediction import Prediction
 
 class MainProgram():
 
@@ -11,6 +14,8 @@ class MainProgram():
         plot_object.create_legends()
         plot_object.plot_legends()
         plot_object.plot_states()
+        #prediction_object = Prediction()
+        #print(prediction_object.test())
 
 def main():
     parser = argparse.ArgumentParser(description='Input a year and return figure of presidential election data')
@@ -19,12 +24,17 @@ def main():
 
     args = parser.parse_args()
     input_year = getattr(args, 'in')
+    valid_years = ['1976', '1980', '1984', '1988', '1992', '1996',
+                   '2000', '2004', '2008', '2012', '2016']
 
     # use input_year, otherwise default to latest election (2016)
-    if input_year:
-        MainProgram(input_year)
-    else:
+    if not input_year:
         MainProgram(2016)
+    elif input_year in valid_years:
+        MainProgram(input_year)
+    elif input_year not in valid_years:
+        print('Please input a valid election year from 1976-2016')
+        sys.exit()
 
 if __name__ == '__main__':
     main()
