ce83e455b8d5391ca3374b908877bb765769b89a
tristanherink13 <herink.tristan@gmail.com>
2020-09-10 18:19:45 -0400

    add Prediction class

---
+++ prediction.py
@@ -0,0 +1,20 @@
+from calculations import Calculations
+
+class Prediction(): #Calculations):
+
+    def test(self):
+        print(self.overall_education_df)
+    
+    '''
+    def __init__(self):
+        # education data
+        data_object = Calculations()
+        overall_education_df = data_object.overall_education_df
+        rural_education_df = data_object.rural_education_df
+        urban_education_df = data_object.urban_education_df
+        # income data
+        income_df = data_object.income_df
+        # demographic_data
+        demographic_df_1976 = data_object.demographic_df_1976
+        demographic_df_1980 = data_object.demographic_df_1980
+    '''
