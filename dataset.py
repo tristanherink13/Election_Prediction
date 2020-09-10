cf3b5df65c155902ec29060cafea2ef6aa9cbc03
tristanherink13 <herink.tristan@gmail.com>
2020-09-10 18:14:46 -0400

    add fcn for re-use in parent class

--- dataset.py
+++ dataset.py
@@ -1,9 +1,20 @@
+import pandas as pd
+import geopandas as gpd
+import ast
+
 class Dataset:
 
-    def __init__(self, file_path):
+    def read_data(self, file_path):
         self.file_path = file_path
-        print('importing {} dataset...'.format(self.file_path.split('/')[-1]))
-    
-    def get_df_columns(self):
-        self.df_columns = self.df.columns.to_list()
-        return self.df_columns
+
+    def convert_to_df(self):
+        short_file_name = self.file_path.split('/')[-1]
+        print('importing {} dataset...'.format(short_file_name))
+
+        if short_file_name[-3:] == 'csv':
+            self.df = pd.read_csv(self.file_path)
+        elif short_file_name[-3:] == 'shp':
+            self.gpd = gpd.read_file(self.file_path)
+        elif short_file_name[-3:] == 'txt':
+            with open(self.file_path, 'r') as data:
+                self.electoral_votes_per_state_per_year_dict = ast.literal_eval(data.read())
