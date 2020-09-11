4b5b603a38d0c804cdd1a82b043e72aa9efa8bdb
tristanherink13 <herink.tristan@gmail.com>
2020-09-10 23:13:53 -0400

    add data to Prediction __init__

--- prediction.py
+++ prediction.py
@@ -1,20 +1,34 @@
 from calculations import Calculations
 
-class Prediction(): #Calculations):
+import sys
+import os
+import pandas as pd
 
-    def test(self):
-        print(self.overall_education_df)
+class Prediction(Calculations):
     
-    '''
     def __init__(self):
+        # get Calculations attributes
+        prediction_data_object = Calculations()
+        prediction_data_object.get_prediction_data()
+        prediction_data_object.get_plotting_data()
         # education data
-        data_object = Calculations()
-        overall_education_df = data_object.overall_education_df
-        rural_education_df = data_object.rural_education_df
-        urban_education_df = data_object.urban_education_df
+        self.overall_education_df = prediction_data_object.overall_education_df
+        self.rural_education_df = prediction_data_object.rural_education_df
+        self.urban_education_df = prediction_data_object.urban_education_df
         # income data
-        income_df = data_object.income_df
-        # demographic_data
-        demographic_df_1976 = data_object.demographic_df_1976
-        demographic_df_1980 = data_object.demographic_df_1980
-    '''
+        self.income_df = prediction_data_object.income_df
+        # demographic data
+        self.demographic_df_1976 = prediction_data_object.demographic_df_1976
+        self.demographic_df_1980 = prediction_data_object.demographic_df_1980
+        self.demographic_df_1984 = prediction_data_object.demographic_df_1984
+        self.demographic_df_1988 = prediction_data_object.demographic_df_1988
+        self.demographic_df_1992 = prediction_data_object.demographic_df_1992
+        self.demographic_df_1996 = prediction_data_object.demographic_df_1996
+        self.demographic_df_2000 = prediction_data_object.demographic_df_2000
+        self.demographic_df_2004 = prediction_data_object.demographic_df_2004
+        self.demographic_df_2008 = prediction_data_object.demographic_df_2008
+        self.demographic_df_2012 = prediction_data_object.demographic_df_2012
+        self.demographic_df_2016 = prediction_data_object.demographic_df_2016
+        self.demographic_df_2018 = prediction_data_object.demographic_df_2018
+        # historical voting data
+        self.rep_dem_state_df = pd.read_csv(os.path.join(sys.path[0], 'Datasets', 'election_data_1976_2016', 'dem_rep_state_votes.csv'))
