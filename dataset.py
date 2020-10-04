693abaf6ee47fe75ea67967beb959a8022520a4d
tristanherink13 <herink.tristan@gmail.com>
2020-10-04 16:58:07 -0400

    add senate/house df formatting and pd.read_csv encoding options

--- dataset.py
+++ dataset.py
@@ -14,7 +14,12 @@ class Dataset:
         print('importing {} dataset...'.format(short_file_name))
 
         if short_file_name[-3:] == 'csv':
-            self.df = pd.read_csv(self.file_path)
+            if 'prediction_data' in short_file_name:
+                self.df = pd.read_csv(self.file_path, encoding = 'ISO-8859-1', dtype=object)
+            elif 'acronym' in short_file_name or 'predictions' in short_file_name:
+                self.df = pd.read_csv(self.file_path)
+            else:
+                self.df = pd.read_csv(self.file_path, encoding = 'ISO-8859-1')
         elif short_file_name[-3:] == 'shp':
             self.gpd = gpd.read_file(self.file_path)
         elif short_file_name[-3:] == 'txt':
@@ -24,17 +29,34 @@ class Dataset:
     def drop_state_column(self, df):
         if 'State' in self.df.columns:
             self.df = self.df.drop(columns=['State'])
-
-    '''
-    def convert_acronyms_to_states(self, df):
-        state_acro_df = pd.read_csv(os.path.join(sys.path[0], 'Datasets', 'state_acronym.csv'))
-        state_list = state_acro_df['State'].tolist()
-        acro_list = state_acro_df['Acronym'].tolist()
-        code_list = state_acro_df['Code'].tolist()
-        df_columns_list = []
-        [df_columns_list.append(header.lower()) for header in self.df.columns.tolist()]
-
-        # convert acronyms to state names
-        for i, state in enumerate(state_list):
-            self.df = self.df.replace(acro_list[i], state)
-    '''
+    
+    def drop_senate_house_cols(self, df):
+        # initialize all state codes and years
+        codes = [1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,
+                25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,
+                45,46,47,48,49,50,51,53,54,55,56]
+        years = [1976, 1978, 1980, 1982, 1984, 1986, 1988,
+                 1992, 1996, 1998, 2000, 2002, 2004, 2006,
+                 2008, 2010, 2012, 2014, 2016, 2018]
+        # drop unneccesary columns
+        self.df = self.df.drop(columns=['state_cen', 'state_ic', 'district', 'stage', 'special',
+                                        'candidate', 'writein', 'mode', 'unofficial', 'version',
+                                        'state', 'state_po', 'office'])
+        # iterate through and fill in states with no senate/house votes for a specific year
+        for year in years:
+            year_df = self.df[self.df['Year'] == year]
+            df_code_list = year_df['Code'].unique()
+            for code in codes:
+                if code not in df_code_list:
+                    self.df.loc[-1] = [year, code, 'nothing', 0, 0]
+                    self.df.index = self.df.index + 1
+                    self.df = self.df.sort_index()
+        # re-map dem/rep/other to 0/1/2
+        if 'sen_party' in self.df.columns:
+            self.df['sen_party'] = self.df['sen_party'].map({'democrat': 0, 'republican': 1})
+            self.df['sen_party'] = self.df['sen_party'].fillna(2)
+            self.df = self.df.groupby(['sen_party', 'Year', 'Code'])[['sen_candidatevotes', 'sen_totalvotes']].sum().reset_index()
+        elif 'house_party' in self.df.columns:
+            self.df['house_party'] = self.df['house_party'].map({'democrat': 0, 'republican': 1})
+            self.df['house_party'] = self.df['house_party'].fillna(2)
+            self.df = self.df.groupby(['house_party', 'Year', 'Code'])[['house_candidatevotes', 'house_totalvotes']].sum().reset_index()
