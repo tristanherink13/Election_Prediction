2e8fcce5293e90a25b3cea53643bf1f639469321
tristanherink13 <herink.tristan@gmail.com>
2020-10-15 21:56:10 -0400

    add comments and set default run to 2020

--- main_program.py
+++ main_program.py
@@ -9,18 +9,21 @@ class MainProgram():
 
     def __init__(self, year):
         self.year = int(year)
-        #plot_object = StateMap(self.year)
-        #plot_object.merge_historical_data_into_gpd()
-        #plot_object.create_legends()
-        #plot_object.plot_legends()
-        #plot_object.plot_states()
-        prediction_object = Prediction()
+        plot_object = StateMap(self.year)
+        plot_object.merge_historical_data_into_gpd()
+        plot_object.create_legends()
+        plot_object.plot_legends()
+        plot_object.plot_states()
+        # prediction methods
+        '''
+        #prediction_object = Prediction()
         #prediction_object.merge_prediction_data()
         #prediction_object.impute_missing_data()
         #prediction_object.compare_multiple_models()
         #prediction_object.run_best_model()
         #prediction_object.plot_confusion_matrix()
         #prediction_object.compute_results()
+        '''
 
 def main():
     parser = argparse.ArgumentParser(description='Input a year and return figure of presidential election data')
@@ -30,11 +33,11 @@ def main():
     args = parser.parse_args()
     input_year = getattr(args, 'in')
     valid_years = ['1976', '1980', '1984', '1988', '1992', '1996',
-                   '2000', '2004', '2008', '2012', '2016']
+                   '2000', '2004', '2008', '2012', '2016', '2020']
 
-    # use input_year, otherwise default to latest election (2016)
+    # use input_year, otherwise default to prediction (2020)
     if not input_year:
-        MainProgram(2016)
+        MainProgram(2020)
     elif input_year in valid_years:
         MainProgram(input_year)
     elif input_year not in valid_years:
